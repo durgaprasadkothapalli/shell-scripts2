43c2168fa13745a33712a73e0f99b72721b2a713
Durgaprasad Kothapalli <durgaprasadkothapalli908@gmail.com>
2024-06-21 21:27:55 +0530

    add 2nd script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,8 @@
 #/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
 
-echo "Ramesh :Hello Suresh,good morning"
-echo "Suresh :Hi Ramesh,very good morning"
-echo "Ramesh :How are you Suresh "
-echo "Suresh :I am good Ramesh,how are you"
+echo "PERSON1 :Hello Suresh,good morning"
+echo "PERSON2 :Hi Ramesh,very good morning"
+echo "PERSON1 :How are you Suresh "
+echo "PERSON2 :I am good Ramesh,how are you"

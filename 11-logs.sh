ebc352fbc0712fe4b13d903e0f0a69ac2969e9fe
Durgaprasad Kothapalli <durgaprasadkothapalli908@gmail.com>
2024-06-24 21:09:59 +0530

    add 8 scripts

---
+++ 11-logs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+ID=$($id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+LOGFILE="/tmp/$0-$TIMESTAMP
+
+VALIDATE( ){
+
+if [ $1 -ne 0 ]
+then
+  echo "error::$2 is failed"
+  exit 1
+else    
+   echo "$2 is failed"
+ fi
+}
+
+ID=$($id -u)
+
+if [ $ID -ne 0 ]
+then
+  echo "error::please run the script"
+  exit 1
+else    
+   echo "you are root user"
+fi
+
+yum install git -y
+
+VALIDATE $?"git installation is sucess"
+
+
+yum install mysql -y
+
+VALIDATE $?"mysql installation is sucess"

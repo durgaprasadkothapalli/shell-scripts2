cc109f6f30aa56151241952df75b7611e969b6f6
Durgaprasad Kothapalli <durgaprasadkothapalli908@gmail.com>
2024-06-24 16:21:27 +0530

    add 8 scripts

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -13,9 +13,9 @@ yum install git -y
 
 if [ $? -ne 0 ]
 then
-echo "error::installing mysql is failed"
+echo "error::installing git is failed"
 exit 1
-else"installing mysql is sucess"
+else"installing git is sucess"
 fi
 
 yum install mysql -y

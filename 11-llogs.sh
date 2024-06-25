4fb954301547a5989b93485406e343568624fbe3
Durgaprasad Kothapalli <durgaprasadkothapalli908@gmail.com>
2024-06-25 13:39:54 +0530

    11 th shell scripting

--- 11-llogs.sh
+++ 11-llogs.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
-ID=$($id -u)
-
+ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
-LOGFILE="/tmp/$0-$TIMESTAMP.log" 
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
 
-VALIDATE( ){
-
-if [ $1 -ne 0 ]
-then
-  echo "error::$2 is failed"
-  exit 1
-else    
-   echo "$2 is failed"
- fi
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "ERROR:: $2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
 }
 
 if [ $ID -ne 0 ]
 then
-  echo "error::please run the script"
-  exit 1
-else    
-   echo "you are root user"
-fi
-
-yum install git -y &>> $LOGFILE
-
-VALIDATE $?"git installation is sucess"
-
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
 
 yum install mysql -y &>> $LOGFILE
 
-VALIDATE $?"mysql installation is sucess"
+VALIDATE $? "Installing MySQL"
 
+yum install git -y &>> $LOGFILE
+
+VALIDATE $? "Installing GIT"

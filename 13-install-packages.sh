a280f416c257a9f256fee81c17a3f497107ea9e5
Durgaprasad Kothapalli <durgaprasadkothapalli908@gmail.com>
2024-06-25 21:39:48 +0530

    11 th shell scripting

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -2,6 +2,27 @@
 
 ID=$(id -u)
 
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "scriptstartted executing at $TIMESTAMP"&>. $LOGFILE
+
+VALIDATE(){
+    IF [ $1 -NE 0 ]
+    then
+    echo -e"$2.....$R failed $N"
+    else
+    echo-e "$2....$g sucess $N"
+
+    fi
+}
+
 if [ $ID -ne 0 ]
 then
 echo "error ;; please run the script with root acess"
@@ -9,14 +30,21 @@ exit 1
 else
 echo " you are root acess"
 fi
-echo "all arguments passed $@"
 
-yum installi mysql -y
+#echo "all arguments passed $@"
+# git mysql postfix net-tools
+#packages=git for first time
 
-if [ $? -ne 0 ]
-then
-    echo "ERROR:: Installing MySQL is failed"
-    exit 1
-else
-    echo "Installing MySQL is SUCCESS"
-fi
+for package in $@
+do
+  
+  yum  list install $package &>> $  LOGFILE # check installed or not
+   if [ $? -ne 0 ] 
+   then
+     yum install $package -y  &>> $LOGFILE
+     VALIDATE $? "installation of $package " #validate
+
+     else
+     echo -e"$packages" is already installed ...$Y skipping $N"
+     fi
+     done

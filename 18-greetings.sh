203225da93e5d6f043a7ddd18167bec471494ef8
Durgaprasad Kothapalli <durgaprasadkothapalli908@gmail.com>
2024-07-16 22:37:16 +0530

    shell-scripts2

--- 18-greetings.sh
+++ 18-greetings.sh
@@ -4,18 +4,18 @@ NAME=""
 WISHES=""
 
 USAGE(){
-    echo"USAGE:: $(basename $0) -n <name> -w <wishes>"
+    echo "USAGE:: $(basename $0) -n <name> -w <wishes>"
     echo "Options::"
     echo "-n, Specify the name (mandatory)"
     echo " -w, Specify the wishes . ex, Good Morning"
     echo "-h, Display help and exit "
 
-    }
+ }
 
     while getopts ":n :w:h"opt; do
-    case $opt in
-    n)NAME="$OPTARG";;
-    W) WISHES="$OPTARG";;
-    h|*)USAGE;exit;;
-esac
-done
+        case $opt in
+           n) NAME="$OPTARG";;
+           W) WISHES="$OPTARG";;
+            h|*) USAGE;exit;;
+         esac
+     done
